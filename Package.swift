b3d27cd2b1cc91841308550d8126ecf8c521d6f7
Allison Husain <allison.husain@berkeley.edu>
2021-05-15 15:03:42 -0600

    v3.0.224

--- Package.swift
+++ Package.swift
@@ -1,8 +1,8 @@
 // swift-tools-version:5.3
 import PackageDescription
 
-let versionTagString = "v3.0.215"
-let checksum = "f5c966e94b6ab23d939ff8eecad88768b619d2f3ea5c5771e14427acf26d6caf"
+let versionTagString = "v3.0.224"
+let checksum = "a6d265e01c9189be1038d03ab9f686a832a3325b7eeab3d4cd280074f799a9f3"
 let package = Package(
     name: "sendbird-ios-xcframework",
     platforms: [
--- generate_release.sh
+++ generate_release.sh
@@ -1,37 +1,24 @@
-make_links() {
-    for target in "universal-simulator" "universal-hardware"; do
-        rm -rf "../SendBirdSDK.xcframework/$target" > /dev/null 2>&1 &&
-        cp -r ../components "../SendBirdSDK.xcframework/$target"
-        #ln -s "components" "../$target"
-        #retval=$?
-        #if [ $retval -ne 0 ]; then
-        #    echo "Symlink failed"
-        #    return $retval
-        #fi
-    done
-    return
-}
-
 rm -rf components > /dev/null 2>&1
 mkdir  components
 cd components
 echo "Downloading latest..."
-curl -LO https://github.com/sendbird/sendbird-ios-framework/releases/latest/download/SendBirdSDK.framework.zip &&
+curl -LO https://github.com/sendbird/sendbird-ios-framework/releases/latest/download/SendBirdSDK.xcframework.zip &&
 echo "Unzipping..." &&
-unzip SendBirdSDK.framework.zip &&
+unzip SendBirdSDK.xcframework.zip &&
 echo "Unpacking..." &&
-mv Carthage/Build/iOS/SendBirdSDK.framework SendBirdSDK.framework &&
-mkdir dSYMs &&
-mv Carthage/Build/iOS/SendBirdSDK.framework.dSYM dSYMs/SendBirdSDK.framework.dSYM &&
-echo "Creating links..." &&
-make_links
-echo "Cleaning before packaging..." &&
-rm -rf Carthage &&
-rm -rf SendBirdSDK.framework.zip &&
-echo "Generating binary resource package..." &&
-cd ../ &&
-zip -r SendBirdSDK.xcframework.zip SendBirdSDK.xcframework &&
-echo "Done"
+#We need to search for the SDK since, for whatever reason, some builds have a zip file that has the entire developer's folder structure in them
+sdk_root_folder=$(find . -type d -name 'SendBirdSDK.xcframework' -print -quit)
+if [ -z "$sdk_root_folder" ]; then
+    echo "Could not find SendBirdSDK.xcframework";
+    exit 1
+fi
 
-echo "To update the package, replace the hash below in Package.swift, update the URL to the expected URL for the new version, commit and push, and then create a release on Github"
+mv "$sdk_root_folder" SendBirdSDK.xcframework &&
+echo "Cleaning before packaging..." &&
+rm -rf SendBirdSDK.xcframework.zip &&
+echo "Generating binary resource package..." &&
+zip -r SendBirdSDK.xcframework.zip SendBirdSDK.xcframework &&
+echo "Done" &&
+
+echo "To update the package, replace the hash below in Package.swift, update the URL to the expected URL for the new version, commit and push, and then create a release on Github" &&
 shasum -a 256 SendBirdSDK.xcframework.zip
